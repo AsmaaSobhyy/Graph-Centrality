b34b8db21ac58dffc211b0bda68ee4e7ca03e17a
Shoroukmohamed <36273234+Shoroukmohamed@users.noreply.github.com>
2019-06-24 18:49:05 +0200

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -22,8 +22,9 @@ public:
 	void addEdge(int u,int v, int wt);
 
 	// prints BFS traversal from a given source s 
-	void BFS(int s);
+	int BFS(int s);
 	void print();
+	int size(int s);
 };
 Graph::Graph(int V,int E)
 {
@@ -37,7 +38,7 @@ void Graph::addEdge(int u,int v, int wt)
 	adj[v].push_back(make_pair(u, wt));
 }
 
-void Graph::BFS(int s)
+int Graph::BFS(int s)
 {
 	// Mark all the vertices as not visited 
 	bool *visited = new bool[V];
@@ -45,17 +46,17 @@ void Graph::BFS(int s)
 		visited[i] = false;
 
 	// Create a queue for BFS 
-	list<int> queue;
-
+	//list<int> queue;
+	int count = 0;
 	visited[s] = true;
-	queue.push_back(s);
+	//queue.push_back(s);
 
-	while (!queue.empty())
-	{
+	//while (!queue.empty())
+	//{
 		// Dequeue a vertex from queue and print it 
-		s = queue.front();
-		cout << s << " ";
-		queue.pop_front();
+		//s = queue.front();
+		//cout << s << " ";
+		//queue.pop_front();
 		pair<int, int>b;
 		for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
 		{
@@ -63,21 +64,32 @@ void Graph::BFS(int s)
 			if (!visited[b.first])
 			{
 				visited[b.first] = true;
-				queue.push_back(b.first);
+				count++;
+				//queue.push_back(b.first);
 			}
-		}
+		//}
 	}
+	return count;
 }
+/*void Graph::size(int s) {
+	for(auto i = adj[s].begin(); i != adj[s].end(); ++i)
+	         n=adj[s].size();
+	return n;
+}*/
+
 int main()
 {
-	Graph g(4,5);
+	Graph g(6,9);
+	g.addEdge(3, 0, 2);
 	g.addEdge(0, 1,1);
 	g.addEdge(0, 2,3);
-	g.addEdge(1, 2,5);
-	g.addEdge(2, 0,3);
-	g.addEdge(2, 3,2);
-	g.addEdge(3, 3,3);
-	g.BFS(2);
+	g.addEdge(2, 1,5);
+	g.addEdge(2, 1,3);
+	g.addEdge(4, 3, 10);
+	g.addEdge(5, 4, 13);
+	g.addEdge(0, 4, 32);
+	g.addEdge(3, 2,3);
+	cout<<g.BFS(3);
 
 	return 0;
 }
