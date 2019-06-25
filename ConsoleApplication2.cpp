af20da2b5cde54abc4b507882ff8da8f5e433e94
asmaa sobhy <asmaasobhy1995@gmail.com>
2019-06-25 19:41:07 +0200

    shortest distance

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,7 +1,7 @@
 // ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
+//#include "pch.h"
 #include <iostream>
 #include<vector>
 #include<list>
--- main.cpp
+++ main.cpp
@@ -1,11 +1,52 @@
 
-
 #include <iostream>
 #include <vector>
 #include <list>
+#include <limits>
+
 
 using namespace std;
 
+//to do 
+
+void Degree(vector<vector<pair<int,int> > > adj_list);
+void Closeness(vector<vector<pair<int,int> > > adj_list);
+void Betweenness(vector<vector<pair<int,int> > > adj_list);
+
+
+
+//-------------------------------------------------------
+vector<int> shortest_distance(int src,
+	vector<vector<pair<int,int> > > adj_list){
+
+	int n = adj_list.size();
+	vector<int>	dis(n, numeric_limits<int>::max()); 
+	//vector<int> p(n, -1);
+	
+	dis[src] = 0;
+	for(int it=0; it<n-1; it++){
+		// Loop over all edges
+				// Loop over all edges
+		for(int u=0; u<n; u++)
+		{
+			for(int e=0; e<adj_list[u].size(); e++)
+			{
+				int v = adj_list[u][e].first;
+				int w = adj_list[u][e].second;
+				
+				if(dis[u] + w < dis[v])
+				{
+					dis[v] = dis[u] + w;
+					//p[v] = u;
+				}
+			}
+		}
+	}
+	
+	return dis;
+    //return p;
+}
+//--------------------------------------------------------------
 
 
 
@@ -13,13 +54,18 @@ main(){
     int nodes,edges,n1,n2,val;
     cin>>nodes>>edges;
     //-------vector(nodes)//list(connected nodes)//pair(no of node connected,weight)----
-    //vector< vector< pair<int,int> > > adj_list( nodes, vector< pair <int, int> > ());       //using vector of vectors 
-    vector< list< pair<int,int> > > adj_list( nodes, list< pair <int, int> > ());        //using vector of lists
+    vector< vector< pair<int,int> > > adj_list( nodes, vector< pair <int, int> > ());       //using vector of vectors 
+    //vector< list< pair<int,int> > > adj_list( nodes, list< pair <int, int> > ());        //using vector of lists
     for(int i=0;i<edges;i++){
         cin>>n1>>n2>>val;
         adj_list[n1].push_back({n2,val});
         adj_list[n2].push_back({n1,val});
 
+    }
+
+    vector<int> x = shortest_distance(0,adj_list);
+    for(int i =0;i<nodes;i++){
+        cout<<x[i]<<endl;
     }
     
     //--------testing----------------
